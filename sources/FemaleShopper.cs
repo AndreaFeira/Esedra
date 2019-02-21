1d2f22cf40952ec086248fb322a8aef5b3e2079c
afeira <afeira@topcon.com>
2019-02-21 23:47:49 +0100

    Manual dependency injection using getter and setter

---
+++ sources/FemaleShopper.cs
@@ -0,0 +1,24 @@
+﻿using Esedra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esedra
+{
+    public class FemaleShopper
+    {
+        public ICreditCard creditCard { get; set; }
+
+        public FemaleShopper()
+        {
+        }
+
+        public string charge()
+        {
+            return creditCard.charge();
+        }
+
+    }
+}
--- sources/Program.cs
+++ sources/Program.cs
@@ -11,6 +11,10 @@ namespace Esedra
     {
         public static void Main(string[] args)
         {
+            //////////////////////////////////////////////
+            //Manual dependency injection using COSTRUCTOR
+            //////////////////////////////////////////////
+
             ICreditCard visa = new Visa();
             ICreditCard masterCard = new MasterCard();
 
@@ -20,6 +24,21 @@ namespace Esedra
             Console.WriteLine(string.Format("Shopper1 is {0}", s1.charge()));
             Console.WriteLine(string.Format("Shopper2 is {0}", s2.charge()));
 
+
+
+            //////////////////////////////////////////////
+            //Manual dependency injection using getter and setter
+            //////////////////////////////////////////////
+            ICreditCard femaleVisa = new Visa();
+            ICreditCard femaleMasterCard = new MasterCard();
+
+            FemaleShopper fs = new FemaleShopper();
+
+            fs.creditCard = femaleVisa;
+            Console.WriteLine(string.Format("FemaleShopper is now {0}", fs.charge()));
+
+            fs.creditCard = femaleMasterCard;
+            Console.WriteLine(string.Format("FemaleShopper is now {0}", fs.charge()));
 
             Console.ReadKey();
         }
