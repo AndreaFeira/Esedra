dcaba55b92c15d1acb67627564fdebd435870eda
afeira <afeira@topcon.com>
2019-02-22 23:20:02 +0100

    Inject dependency using a ICreditCard Resolver over a configuration

---
+++ sources/CreditCardResolver.cs
@@ -0,0 +1,24 @@
+﻿using Esedra.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esedra
+{
+    public static class CreditCardResolver
+    {
+        //Resolve dependency over a configuration
+        public static ICreditCard resolve()
+        {
+            string defaultCreditCardType = Properties.Settings.Default.ConfiguredCreditCard;
+
+            if ("Visa" == defaultCreditCardType)
+                return new Visa();
+            else
+                return new MasterCard();
+        }
+    }
+}
--- sources/Program.cs
+++ sources/Program.cs
@@ -11,44 +11,12 @@ namespace Esedra
     {
         public static void Main(string[] args)
         {
-            //////////////////////////////////////////////
-            //Manual dependency injection using COSTRUCTOR
-            //////////////////////////////////////////////
+            //Using a resolver to remove all concrete dependency of the shopper
+            ICreditCard creditCard = CreditCardResolver.resolve();
 
-            ICreditCard visa = new Visa();
-            ICreditCard masterCard = new MasterCard();
+            Shopper shopper = new Shopper(creditCard);
 
-            Shopper s1 = new Shopper(masterCard);
-            Shopper s2 = new Shopper(visa);
-
-            Console.WriteLine(string.Format("Shopper1 is {0}", s1.charge()));
-            Console.WriteLine(string.Format("Shopper2 is {0}", s2.charge()));
-
-
-            //////////////////////////////////////////////
-            //Manual dependency injection using getter and setter
-            //////////////////////////////////////////////
-            ICreditCard femaleVisa = new Visa();
-            ICreditCard femaleMasterCard = new MasterCard();
-
-            FemaleShopper fs = new FemaleShopper();
-
-            fs.creditCard = femaleVisa;
-            Console.WriteLine(string.Format("FemaleShopper is now {0}", fs.charge()));
-
-            fs.creditCard = femaleMasterCard;
-            Console.WriteLine(string.Format("FemaleShopper is now {0}", fs.charge()));
-
-
-            //////////////////////////////////////////////
-            //Manual dependency injection using Interfaces
-            //////////////////////////////////////////////
-            CompulsiveShopper compulsiveShopper = new CompulsiveShopper();
-            ICreditCard cardWithNoMoney = new Visa();
-
-            ((ICreditCardDependent)compulsiveShopper).InjectADependency(cardWithNoMoney);
-
-            Console.WriteLine(string.Format("Compulsive shopper is now {0}", compulsiveShopper.charge()));
+            Console.WriteLine(string.Format("Shopper is {0}", shopper.charge()));
 
             Console.ReadKey();
         }
